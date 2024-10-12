decf3ac0e36e8f4a667b040a3a22c7cb09b43947
AdrianLlontop <138237630+adrianzg12@users.noreply.github.com>
2024-10-12 00:59:49 -0500

    error arreglado

--- src/main/java/pe/edu/cibertec/DAWIISpringSecurityClient/config/SecurityConfig.java
+++ src/main/java/pe/edu/cibertec/DAWIISpringSecurityClient/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/general").permitAll()
+                        auth.requestMatchers("/api/paciente").permitAll()
                                 .requestMatchers("/api/gestor").authenticated()
                                 .requestMatchers("/api/coordinador").authenticated()
                 )
